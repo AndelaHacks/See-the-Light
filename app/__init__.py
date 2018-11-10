c20ad31e827dc03324a7bc784ab3e489a61079c4
Charles Njenga <mwangicharles@gmail.com>
2018-11-10 10:53:35 +0300

    Cleaning up code

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask, Blueprint
 from app.api.views.news import news
-# from app.api.v1.views.sales import sales
-# from app.api.v1.views.products import products
 # from app.api.v1.views.errors import errors
 
 
@@ -9,5 +7,4 @@ app = Flask(__name__)
 app.register_blueprint(news, url_prefix='/news')
 # app.register_blueprint(news, url_prefix='/')
 # app.register_blueprint(news, url_prefix='/news')
-# app.register_blueprint(products, url_prefix='/api/v1')
 # app.register_blueprint(errors)
--- app/api/views/news.py
+++ app/api/views/news.py
@@ -1,73 +1,29 @@
 from flask import Flask, Blueprint, jsonify, request
-from app.api.models import news_model
-import json
+# from app.api.models import news_model
+# import json
 import requests
-# from app import app
-# from app.api.v1.models.news_model import Sale
-# from app.api.v1.models.store_model import Store
+
 news = Blueprint('news', __name__)
 app = Flask(__name__)
+
 
 @news.route('/')
 def hello():
     return "This is a project by team STL: sample link(http://127.0.0.1:5000/news/get?link=https://www.bbc.com/news/uk-politics-46155403)"
 
-# @news.route('/news', methods = ['GET'])
-# def get_all_news():
-#     all_news = Store.get_all_news()
-#     if all_news:
-#         return jsonify(all_news)
-#     else:
-#         return "The sale records are empty"
-@news.route('/get', methods = ['GET'])
+
+@news.route('/get', methods=['GET'])
 def post_news_link():
     link = request.args.get('link')
     print('######## THIS IS THE LINK FROM GET METHOD', link)
     response = requests.post('http://newsbreakers.herokuapp.com',
-        data= {"text":link}
-            # content_type='application/json'
-        )
+                             data={"text": link}
+                             # content_type='application/json'
+                             )
     print("###########", response.content)
     return (response.content)
 
     # return "This is a project by team STL"
-
-# @news.route('/news', methods = ['POST'])
-# def post_news():
-    
-#     #reading the request
-#     req_data = request.get_json()
-#     # sale_id = req_data['sale_id']
-#     # sale_id = "S" + str(len(Store.news) + 1)
-#     # product_id = req_data['product_id']
-#     # quantity_sold = req_data['quantity_sold']
-#     # amount = req_data['amount']
-#     link = req_data['link']
-#     print("####### THE LINK PROVIDED IS ###########", link)
-#     # response = app.test_client.post('/api/v1/products')
-
-
-#     #creating sale object
-#     # sale = Sale(sale_id, product_id, quantity_sold, amount)
-#     # store = Store()
-#     # response = sale.post_sale()
-#     # if response == 'success':
-#     #     #update the records of the products by substracting sold qty
-#     #     try:
-#     #         Store.update_product(product_id, -(int(quantity_sold)))
-#     #     except:
-#     #         print ("No such record-temp error message")
-#     #     return 'sale transaction successfully processed', 201
-#     # else:
-#     #     return 'the sale transaction did not go through, possible invalid request', 400
-
-#     response = requests.post('http://newsbreakers.herokuapp.com',
-#         data= {"text":link}
-#             # content_type='application/json'
-#         )
-
-#         # name = TextField('Name:', validators=[validators.required()])
-#         # form = ReusableForm(request.form)
 
 #     # response = app.test_client().post(
 #     #     'newsbreakers.herokuapp.com/',
