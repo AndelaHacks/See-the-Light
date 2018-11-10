91dc7964f293ce07640eb72f620cc0022ce4e230
Charles Njenga <mwangicharles@gmail.com>
2018-11-10 08:01:14 +0300

    mIP reputation logic

--- IP_Address_Lookup.py
+++ IP_Address_Lookup.py
@@ -2,7 +2,7 @@ import socket
 import dns.resolver
 
 # Remove the below line when finally intergrated with main app
-link = 'https://nation.co.ke/bla/bla/some-other-link.html'
+link = "https://www.kahawatungu.com/category/entertainment/"
 
 # Split the link and get element 2
 get_url = (link.split('/'))[2]
@@ -12,41 +12,21 @@ get_url = (link.split('/'))[2]
 # choose from below 3 methods the one that is optimal
 
 # method 1
-answers = dns.resolver.query(get_url)
-for IP_Addresses in answers:
-    print(IP_Addresses)
+# answers = dns.resolver.query(get_url)
+# for IP_Addresses in answers:
+#     print(IP_Addresses)
 
 # method 2
 addr = socket.gethostbyname(get_url)
-print(addr)
+print(IP_addr)
 
-# method 3
+key_pass = 'Mwangi2018'
 
-
-def get_records(domain):
-    """
-    Get all the records associated to domain parameter.
-    :param domain:
-    :return:
-    """
-    ids = [
-        'A',
-        'NS',
-        'CNAME',
-        'SOA',
-        'MX',
-        'TXT',
-    ]
-
-    for a in ids:
-        try:
-            answers = dns.resolver.query(domain, a)
-            for rdata in answers:
-                print(a, ':', rdata.to_text())
-
-        except Exception as e:
-            print(e)  # or pass
+GET /badip_batch/IP_addr HTTP/1.1
+Host: api.apility.net
+X-Auth-Token: 52a90970-cfa0-4536-91e5-f7148bb25c61
+Cache-Control: no-cache
+Postman-Token: 77a94897-edff-f7bc-7b48-6874d8a37340
 
 
-if __name__ == '__main__':
-    get_records(get_url)
+# $ curl -H "Accept: application/json" -H "X-Auth-Token: UUID" -X GET "https://api.apility.net/badip_batch/<IP1>,<IP2>...<IPn>"
