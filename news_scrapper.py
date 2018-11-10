9b8593b7d8577966cdef8e4e1d871673c8651fd1
Charles Njenga <Hackitect@users.noreply.github.com>
2018-11-10 13:33:49 +0300

    Update news_scrapper.py

--- news_scrapper.py
+++ news_scrapper.py
@@ -1,9 +1,9 @@
 from newspaper import Article
-# replace with dynamic value once the code is working
+# replace with dynamic value once the code is has been integrated on the view.py as its own route
 url = 'https://nation.co.ke/bla/bla/some-other-link.html'
 
 
-# we are going to anaylse a news article URL scrape the domain name and IP address
+# we are going to anaylse a news article URL, scrape the domain name and IP address
 # Return Sensational score for the heading
 # return reputation score from the domain name/IP address using Alexa
 # return keyword analytics from the body text
